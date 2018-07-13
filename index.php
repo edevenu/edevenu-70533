40bbbe46576eeacb1480399a41fc0b992ddedc1e
Deepu Venugopal <deepu.venugopal@ericsson.com>
2018-07-13 15:09:34 +0200

    new PHP file

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <!DOCTYPE html>
 <html>
  <body>
- <?php echo '<p>70-533 webapp Created using my own repo</p>'; ?> 
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2></h2>
+				<p></p>
+				<table class ="table table-stripped">
+					<thead>
+						<tr>
+							<th>Environment</th>
+							<th>Message</th>
+						</tr>
+					<tbody>
+						<tr>
+							<td><?php echo getenv("Env"); ?> </td>
+							<td><?php echo getenv("slotMessage"); ?> </td>
+						</tr>
+					</tbody>
+					</thead>
+				</table>	
+			</div>
+		</div>
+	</div>
  </body>
 </html> 
