951f486252c18d05d560fee73790012d6fbce138
Deepu Venugopal <deepu.venugopal@ericsson.com>
2018-07-13 20:51:36 +0200

    Modified table style

--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 <html>
  <body>
  <style>
-table, th, td {
-    border: 1px solid black;
-}
+	table 
+	{  
+		color: #333; /* Lighten up font color */
+		font-family: Helvetica, Arial, sans-serif; /* Nicer font */
+		width: 640px; 
+		border-collapse: 
+		collapse; border-spacing: 0; 
+	}
+
+	td, th 
+	{ 
+		border: 1px solid #CCC; height: 30px; 
+	} 
+
+	th 
+	{  
+		background: #F3F3F3; /* Light grey background */
+		font-weight: bold; /* Make sure they're bold */
+	}
+
+	td 
+	{  
+		background: #FAFAFA; /* Lighter grey background */
+		text-align: center; /* Center our text */
+	}
 </style>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<h2>70-533 Microsoft Azure Infrastructre Exam</h2>
 				<p>This is demo page created for webapp</p>
-				<table style="width:100%">
+				<table style="width:50%">
 					<thead>
 						<tr>
 							<th>Environment</th>
