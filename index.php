ecf8ad2425be8fa842918ff47bb7a2c8d33815eb
Deepu Venugopal <deepu.venugopal@ericsson.com>
2018-07-14 00:28:57 +0200

    correcting the site name variable

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 					<tbody>
 						<tr>
 							<td><?php echo getenv("Env"); ?> </td>
-							<td><?php echo getenv("WEBSITE_HOSTNAME"); ?> </td>
+							<td><?php echo getenv("WEBSITE_SITE_NAME"); ?> </td>
 							<td><?php echo getenv("slotMessage"); ?> </td>
 						</tr>
 					</tbody>
