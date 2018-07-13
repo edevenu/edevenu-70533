10da2783048169a0b839316932aacd330cf4dfe4
Deepu Venugopal <deepu.venugopal@ericsson.com>
2018-07-13 15:32:08 +0200

    New tabular format

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html>
  <body>
+ <style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h2></h2>
-				<p></p>
-				<table class ="table table-stripped">
+				<h2>70-533 Microsoft Azure Infrastructre Exam</h2>
+				<p>This is demo page created for webapp</p>
+				<table style="width:100%">
 					<thead>
 						<tr>
 							<th>Environment</th>
