47c829fa44bfcf4c86df22b80f252b718c9e3298
Deepu Venugopal <deepu.venugopal@ericsson.com>
2018-07-13 22:08:53 +0200

    New PHP

--- index.php
+++ index.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
 <html>
  <body>
+ <style>
+	table 
+	{  
+		color: #333; /* Lighten up font color */
+		font-family: Helvetica, Arial, sans-serif; /* Nicer font */
+		width: 640px; 
+		border-collapse: 
+		collapse; border-spacing: 0; 
+	}
+
+	td, th 
+	{ 
+		border: 1px solid #CCC; height: 30px; 
+	} 
+
+	th 
+	{  
+		background: #F3F3F3; /* Light grey background */
+		font-weight: bold; /* Make sure they're bold */
+	}
+
+	td 
+	{  
+		background: #FAFAFA; /* Lighter grey background */
+		text-align: center; /* Center our text */
+	}
+</style>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h2></h2>
-				<p></p>
-				<table class ="table table-stripped">
+				<h2>70-533 Microsoft Azure Infrastructre Exam</h2>
+				<p>This is demo page created for webapp</p>
+				<table style="width:50%">
 					<thead>
 						<tr>
 							<th>Environment</th>
@@ -19,7 +46,8 @@
 						</tr>
 					</tbody>
 					</thead>
-				</table>	
+				</table>
+				<p style="background-color:#FF0000;foreground-color:#000000;"><?php echo "This value will be displayed only for devlopment slot :"; getenv("slotOnlyVar"); ?></p>
 			</div>
 		</div>
 	</div>
